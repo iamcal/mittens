80601324bbecc72d06e30d8a38102a04f77f84d1
Cal Henderson <cal@iamcal.com>
2008-05-24 22:12:16 +0000

    use a root urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls.defaults import *
+from mittens import urls
 from mittens import settings
 
-urlpatterns = patterns('',
+urlpatterns = urls.urlpatterns
+urlpatterns += patterns('',
     (settings.APP_ADMIN_PATH, 'mittens.app.views.admin'),
     (r'^$', 'mittens.app.views.mittens'),
 )
--- index/urls.py
+++ index/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls.defaults import *
+from mittens import urls
 from mittens import settings
 
-urlpatterns = patterns('',
+urlpattenrs = urls.urlpatterns
+urlpatterns += patterns('',
     (settings.INDEX_ADMIN_PATH, include('django.contrib.admin.urls')),
     (r'^$', 'mittens.index.views.index'),
 )
