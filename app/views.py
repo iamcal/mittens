bf2a493f72161221d00dded06a8e7818370943d7
Leah Culver <leah.culver@gmail.com>
2008-05-27 22:03:32 +0000

    user admin page

--- app/views.py
+++ app/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render_to_response
 def mittens(request):
     return render_to_response('mittens.html', {
     }, context_instance=RequestContext(request))
+    
+def admin(request):
+    return render_to_response('admin.html', {
+    }, context_instance=RequestContext(request))
