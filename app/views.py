60c3176a2164a73fc9fa964fdfacc73a033ca310
Cal Henderson <cal@iamcal.com>
2008-05-27 23:04:33 +0000

    ignore that last one, kthnks

--- app/views.py
+++ app/views.py
@@ -8,13 +8,21 @@ def mittens(request):
     
 def admin(request, moduleid=0, extra=''):
 
+    module = None
+
     try:
-        link = models.ModuleLink.objects.get(id=moduleid)
-        module = link.module
+        module = models.ModuleLink.objects.get(id=moduleid).module
+    except:
+        print "no object thingy - try and get the default module, then redirect"
+
+    # this is inside try/except because {module} might be None
+    # and extra might be None. still seems to always do the right
+    # thing, but it's U.G.L.Y.
+    try:
         module.admin_request_path = '/' + extra
     except:
-        link = None
-        print "no object thingy - try and get the default module, then redirect"
+        pass
+
 
     return render_to_response('admin.html', {
         'current_module': module,
